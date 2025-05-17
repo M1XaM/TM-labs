e7c9659153ca73248cc29fb31255cec67c6832f0
DannaCojocari <danielacojocari08@gmail.com>
2025-05-18 00:37:38 +0300

    Initiated the inventory

---
+++ item.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if randi() % 2 == 0:
+		$TextureRect.texture = load("res://Assets/farm/carrot/carrot_05.png")
+	else:
+		$TextureRect.texture = load("res://Assets/farm/pumpkin/pumpkin_05.png")
+		
